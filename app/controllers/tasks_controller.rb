8fe17e295daa7716a1e15255707632cda778c860
Cristopher <cristopheer96@gmail.com>
2022-04-03 06:17:19 -0500

    modified search

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,26 +6,31 @@ class TasksController < ApplicationController
 
 
   def index
-    if (params[:task_name] || params[:tag_id] || params[:importt] || params[:start_date] || params[:end_date] )
 
+
+    if (params["/tasks"]  )
+
+      params["/tasks"]["start_date"]
       @task_user = current_user.tasks
       @tasks = @task_user.where(importance: 10) #<ActiveRecord::Relation []> Para que arroje este tipo de dato  no funciona  ActiveRecord::Relation
       @task_task_name = @task_user.where("title ILIKE ?", "%#{params[:task_name]}%")
       case params[:importt]
-      when 'tres' then @task_importance = @task_user.where(importance: 3)
-      when 'dos' then @task_importance = @task_user.where(importance: 2)
-      when 'uno' then @task_importance = @task_user.where(importance: 1)
-      when 'cuatro' then @task_importance = @task_user.where(importance: 4)
-      when 'cinco' then  @task_importance = @task_user.where(importance: 5)
+      when 'tres' then @task_importance = @task_task_name.where(importance: 3)
+      when 'dos' then @task_importance = @task_task_name.where(importance: 2)
+      when 'uno' then @task_importance = @task_task_name.where(importance: 1)
+      when 'cuatro' then @task_importance = @task_task_name.where(importance: 4)
+      when 'cinco' then  @task_importance = @task_task_name.where(importance: 5)
       else
-        @task_importance = @task_user.where(importance: 10)
+        @task_importance = @task_task_name
       end
-      @task_start_date = @task_user.where(start_date: params[:start_date])
-      @task_start_date = @task_user.where(importance: 10) if @task_start_date.nil?
-      @task_end_date = @task_user.where(start_date: params[:end_date])
-      @task_end_date = @task_user.where(importance: 10) if @task_end_date.nil?
 
-      @tasks = @task_task_name + @task_importance + @task_start_date + @task_end_date
+      @task_start_date = @task_importance.where(start_date: "#{params["/tasks"]["start_date"]} 00:00:00.000000000 +0000")
+      # @task_start_date = @task_importance if params["/tasks"]["start_date"] = ""
+
+      @task_end_date = @task_start_date.where(end_date: "#{params["/tasks"]["end_date"] } 00:00:00.000000000 +0000")
+      # @task_end_date = @task_start_date if params["/tasks"]["end_date"] = ""
+      raise
+      @tasks = @task_end_date
       @tasks = @task_user.where(user_id: current_user.id) if @tasks.nil?
       @tasks.uniq
     else
@@ -98,9 +103,27 @@ class TasksController < ApplicationController
       @task = Task.find(params[:id])
   end
 
-  def search_tasks(userio,task_name,tag_id,importt,start_date )
-        if (params[:task_name] || params[:tag_id] || params[:importt] || params[:start_date] || params[:end_date] )
+  def search_tasks(usuario,task_name,tag_id,importt,start_date,end_date )
+      # @task_user = usuario.tasks
+      # @tasks = @task_user.where(importance: 10) #<ActiveRecord::Relation []> Para que arroje este tipo de dato  no funciona  ActiveRecord::Relation
+      # @task_task_name = @task_user.where("title ILIKE ?", "%#{task_name]}%")
+      # case params[:importt]
+      # when 'tres' then @task_importance = @task_user.where(importance: 3)
+      # when 'dos' then @task_importance = @task_user.where(importance: 2)
+      # when 'uno' then @task_importance = @task_user.where(importance: 1)
+      # when 'cuatro' then @task_importance = @task_user.where(importance: 4)
+      # when 'cinco' then  @task_importance = @task_user.where(importance: 5)
+      # else
+      #   @task_importance = @task_user.where(importance: 10)
+      # end
+      # @task_start_date = @task_user.where(start_date: params[:start_date])
+      # @task_start_date = @task_user.where(importance: 10) if @task_start_date.nil?
+      # @task_end_date = @task_user.where(start_date: params[:end_date])
+      # @task_end_date = @task_user.where(importance: 10) if @task_end_date.nil?
 
+      # @tasks = @task_task_name + @task_importance + @task_start_date + @task_end_date
+      # @tasks = @task_user.where(user_id: current_user.id) if @tasks.nil?
+      # @tasks.uniq
   end
 
 end
